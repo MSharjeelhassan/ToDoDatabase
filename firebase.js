a0a891b2159c23e078d61da52e7a724086f3cd4d
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2024-03-19 18:36:47 +0500

    Del task and Edit task remaining

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut  } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
-import { getDatabase, set, ref, onValue, push } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
+import { getDatabase, set, ref, onValue, push, remove } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,5 +25,5 @@ const db = getDatabase(app);
 
 export{
     auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, db, ref, set,
-    onValue, push
+    onValue, push, remove
 }
--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-import{db, ref, set, onValue, push} from "./firebase.js"
+import{db, ref, set, onValue, push, remove} from "./firebase.js"
 
 
 let addItem = document.getElementById('addItem');
@@ -15,10 +15,9 @@ console.log(localStorage);
 welcomeName.innerText = userObj.email.slice(0,userObj.email.indexOf('@'));
 
 
-// +++++++++++++++++ Add task btn  +    ++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++ Add task btn  +   Sending DATA TO DATABASE ++++++++++++++++++++++++++++++++++++++++
 
 addItem.addEventListener('click',()=>{
-    // mainDiv.innerText = "" yeh yousuf bhai ka method hy
     userObj.task = enterTask.value;
 
 let id = push(ref(db,`todo/${userObj.uid}`)).key;
@@ -28,56 +27,22 @@ set(refer,userObj);
 // return id;
 console.log(userObj.task);
 
-// let refer = ref(db,`todo/${userObj.uid}/`);    yeh yousuf na bola tha
-// push(refer,userObj);
-// // return id;
-// console.log(enterTask.value);
-
-// let reference = ref(db,)
-// onValue(refer, (snapshot) => {
-//   const data = snapshot.val();
-// console.log(data);
- 
-
-//   let div = document.createElement('div');
-//   let content = document.createElement('p');
-//   let contentText = document.createTextNode(userObj.task);
-//   content.appendChild(contentText);
-//   div.appendChild(content);
-//   div.className = "d-flex justify-content-start align-item-center container mb-3 gap-2 rounded bg-opacity-50 bg-light"
-//   content.className = "bg-light text-start my-2"
-//   mainDiv.appendChild(div);
-  
-//   let editBtn = document.createElement('button');
-//   let editHeading = document.createTextNode('Edit Task');
-//   editBtn.appendChild(editHeading);
-//   div.appendChild(editBtn);
-//   editBtn.className = "btn btn-primary"
-//   editBtn.setAttribute('onclick','edit(this)');
-  
-//   let delTaskBtn  = document.createElement('button');
-//   let delTaskHeading = document.createTextNode('Delete Task');
-//   delTaskBtn.appendChild(delTaskHeading);
-//   div.appendChild(delTaskBtn);
-//   delTaskBtn.className="btn btn-danger ms-2"
-//   delTaskBtn.setAttribute('onclick','delTask(this)');
-//   enterTask.value = "";
-// });
-
 })
-// +++++++++++++++ yeh bhi experiment tha igonre it+++++++++++++++Start horaha hy +++++++++++++++++++++++++++
+// +++++++++++++++ Getting DATA from DATABASE +++++++++++++++Start horaha hy +++++++++++++++++++++++++++
 
-function getData(){    
-let reference = ref(db,`todo/${userObj.uid}`)
+ 
+let reference = ref(db,`todo/${userObj.uid}`);
 onValue(reference, (snapshot) => {
     let snapshotData = snapshot.val();
+    if(!snapshotData){return};
   console.log(snapshotData);
+  mainDiv.innerHTML = "";
    for(let i=0; i<Object.values(snapshotData).length;i++){
-    console.log(Object.values(snapshotData)[i].value)
+    console.log(Object.values(snapshotData)[i].task)
 
     let div = document.createElement('div');
     let content = document.createElement('p');
-    let contentText = document.createTextNode(Object.values(snapshotData)[i].value);
+    let contentText = document.createTextNode(Object.values(snapshotData)[i].task);
     content.appendChild(contentText);
     div.appendChild(content);
     div.className = "d-flex justify-content-start align-item-center container mb-3 gap-2 rounded bg-opacity-50 bg-light"
@@ -97,22 +62,18 @@ onValue(reference, (snapshot) => {
     div.appendChild(delTaskBtn);
     delTaskBtn.className="btn btn-danger ms-2"
     delTaskBtn.setAttribute('onclick','delTask(this)');
-    enterTask.value = "";
-    // div.innerHTML = "";
+    delTaskBtn.setAttribute('id','userObj.uid');
+        enterTask.value = "";
+ 
    }
   });
-   }
-  
 
-
-getData()
-
-
-// +++++++++++++++ upar wala experiment tha igonre it+++++++++++++++end hogaya hy +++++++++++++++++++++++++++
+// +++++++++++++++ Getting DATA from DATABASE  +++++++++++++++end hogaya hy +++++++++++++++++++++++++++
 
 deleteAll.addEventListener('click',()=>{
     mainDiv.innerHTML = '';
     enterTask.value ="";
+    remove(reference);
 
 })
 
@@ -125,11 +86,12 @@ window.edit= function(a){
 
 window.delTask= function(a){
     a.parentNode.remove();
+   
+
 }
 
 let home = document.getElementById('home');
 home.addEventListener('click',()=>{
-    // alert("go")
 
     window.location = "index.html";
 })
